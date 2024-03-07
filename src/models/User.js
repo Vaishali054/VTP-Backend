d608858f0ac8b95ddb400d9f00a0043f2eb87f07
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2024-03-07 15:26:44 +0530

    fix: user schema

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  email: {
+  email_id: {
     type: String,
     required: true,
     unique: true,
