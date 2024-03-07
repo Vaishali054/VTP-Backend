e1d46cc5ac5fd5cc6707bce5eddd984753f906f9
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2024-03-07 12:43:56 +0530

    add: User model

---
+++ src/models/User.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  accountStatus: {
+    type: String,
+    enum: ['active', 'deleted'],
+    default: 'active',
+  },
+  accountCreationDate: {
+    type: Date,
+    default: Date.now,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  currentBalance: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
