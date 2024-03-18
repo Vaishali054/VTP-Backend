b8616bc5a1c93cd4150a107417810b2cee354e0d
SomyaChawla0250 <119046192+SomyaChawla0250@users.noreply.github.com>
2024-03-18 16:36:05 +0530

    moves constant to separate file and use prettier

---
+++ src/constants/constants.js
@@ -0,0 +1,9 @@
+export const API_FETCH_TIME = 60000;
+export const OPENING_HOUR = 9;
+export const OPENING_MINUTE = 15;
+export const CLOSING_HOUR = 15;
+export const CLOSING_MINUTE = 30;
+export const MONDAY = 1;
+export const FRIDAY = 5;
+
+
