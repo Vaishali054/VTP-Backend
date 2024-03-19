12e2e777a88338237a49a1177c9be3ced559e85b
Tanishka Gupta <130149454+tanishkagupta2207@users.noreply.github.com>
2024-03-19 19:45:58 +0530

    Update portfolioController.js

--- src/controllers/portfolioController.js
+++ src/controllers/portfolioController.js
@@ -4,31 +4,30 @@ import Companies from '../models/Companies.js';
 import Portfolio from '../models/Portfolio.js';
 import { authenticateJWT } from "../middlewares/authJWT.js";
 import { validateUser } from "../middlewares/validateUser.js";
+const ObjectId = mongoose.Types.ObjectId;
 
 export const generatePortfolio = async (req, res) => {
   const userId = req.query.userId;
-  console.log("user: ", userId);
 
   try {
     const Port_stats = await Portfolio.findOne({ User_Id: new mongoose.Types.ObjectId(userId) });
-    console.log("portfolio: ", Port_stats);
+    
 
     if (Port_stats.status !== 'public') {
       authenticateJWT(req, res, async () => {
-        console.log("authorized");
         validateUser(req, res, async () => {
           const userData = req.body.user;
           if (!userData) {
             return res.status(403).json({ message: "Unauthorized" });
           }
           const userStocks = await UserStocks.find({ User_Id: new mongoose.Types.ObjectId(userId) });
-          console.log("stocks: ", userStocks);
+          
           const portfolio = [];
 
           for (const stock of userStocks) {
             const companyDetails = await Companies.findOne({ Company_Id: stock.Company_Id });
-            console.log(companyDetails);
-            const totalValue = stock.numberOfStocks * companyDetails.Current_Price;
+           
+            const totalValue = stock.numberOfStocks * companyDetails.current_Price;
 
             portfolio.push({
               Quantity: stock.numberOfStocks,
@@ -42,13 +41,13 @@ export const generatePortfolio = async (req, res) => {
       });
     } else {
       const userStocks = await UserStocks.find({ User_Id: new mongoose.Types.ObjectId(userId) });
-      console.log("stocks: ", userStocks);
+      
       const portfolio = [];
 
       for (const stock of userStocks) {
         const companyDetails = await Companies.findOne({ Company_Id: stock.Company_Id });
-        console.log(companyDetails);
-        const totalValue = stock.numberOfStocks * companyDetails.Current_Price;
+        
+        const totalValue = stock.numberOfStocks * companyDetails.current_Price;
 
         portfolio.push({
           Quantity: stock.numberOfStocks,
