2859a6a9bf809e2bb6e3390ed4e39256cae2beab
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2024-03-23 13:22:26 +0530

    add : frontend url

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://vtp-frontend.vercel.app/",
     credentials: true,
   })
 );
